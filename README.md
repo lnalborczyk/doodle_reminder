c07c99244d85c10bf89f9fbfaff5519edbdd8722
lnalborczyk <ladislas.nalborczyk@gmail.com>
2018-09-29 13:48:03 +0200

    minor updates

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# Sending automatised email reminder to the participants of a doodle
+# How to send a reminder email to participants of a doodle
+
+During my PhD...
 
 Reshaping an excel file issued from doodle and sending an reminder email the day before the meeting...
+
+## A minimal example
+
+I have set up an example doodle available following this link: https://doodle.com/poll/g7vee2qy2swhmt9g
+
+So far,
+
+## Sending emails from R
+
+Blah blah... https://github.com/jennybc/send-email-with-r
+
+## Scheduling it (for Mac OSX)
+
+Blah blah ... https://forums.macrumors.com/threads/schedule-automator-workflow-or-app.1996548/
+
+NB: this script might need to be adapted a bit if participants of the doodle can choose multiple (more than one) dates and/or if some participants chose no date.
--- automatic_email_reminder.R
+++ automatic_email_reminder.R
@@ -2,14 +2,17 @@
 # Reshaping an excel file issued from doodle
 # and sending an reminder email
 # the day before the meeting
-########################################
+############################################
 
 library(tidyverse)
 library(readxl)
 library(gmailr)
 library(purrr)
 
-# setwd(getSrcDirectory()[1])
+# NB: this won't work if executed from RStudio, but will work if the script if sourced
+# getting the directory of the current script
+
+setwd(getSrcDirectory()[1])
 
 reiterate <- function(x) {
     
@@ -23,146 +26,140 @@ reiterate <- function(x) {
 }
 
 data <-
-    read_excel("/Users/Ladislas/Desktop/M2R_Sonja_2017_2018/doodle.xls") %>%
+    # importing the xls file from doodle
+    read_excel("Doodle.xls") %>%
     # remove the two first lines
     slice(3:nrow(.) ) %>%
-    # remove the last two lines
-    slice(1:(nrow(.)-3) ) %>%
     # remove the first column (pseudo)
     select(-1)
 
-# working on data2 below
-data2 <- data
-data2[1, ] <- reiterate(data2[1, ])
-data2[2, ] <- reiterate(data2[2, ])
-data2[3, 2:ncol(data2)] <- substr(data2[3, 2:ncol(data2)], 1, 5)
+# filling NAs of the first two lines with the last value
+data[1, ] <- reiterate(data[1, ])
+data[2, ] <- reiterate(data[2, ])
 
-# split the month and year by space
-x <- strsplit(as.character(data2[1, ]), " ", fixed = TRUE)
+# keeping only the time of the beginning of the meeting
+data[3, 2:ncol(data)] <- substr(data[3, 2:ncol(data)], 1, 5)
+
+# split the month and year, rbind it and transpose it
+x <- strsplit(as.character(data[1, ]), " ", fixed = TRUE) %>% do.call(rbind, .) %>% t
 
 # rbind it
-xx <- do.call(rbind, x) %>% t
-data2 <- rbind(xx[2,], xx[1,], data2[2:nrow(data2), ])
+data <- rbind(x[2,], x[1,], data[2:nrow(data), ])
 
 # "naming" the first four rows
-data2[1:4, 1] <- c("year", "month", "day", "hour")
+data[1:4, 1] <- c("year", "month", "day", "hour")
 
-# reshaping the hours
-data2[3, 2:ncol(data2)] <- substr(data2[3, 2:ncol(data2)], 6, 10)
-
-# translatig months
-data2[2, 2:ncol(data2)] <- sapply(data2[2, 2:ncol(data2)], FUN = function(x) ifelse(x == "octobre", "October", "November") )
+# extractong the day number
+data[3, 2:ncol(data)] <- substr(data[3, 2:ncol(data)], 4, 6)
 
 # concatenating rows
-data2[1, ] <- t(
+data[1, ] <- t(
     do.call(
-        paste, c(as.data.frame(t(data2[1:3, ]), stringsAsFactors = FALSE), sep = ", ")
+        paste, c(as.data.frame(t(data[1:3, ]), stringsAsFactors = FALSE), sep = ", ")
         )
     )
 
 # transposing it
-data3 <- as.data.frame(t(data2), stringsAsFactors = FALSE)
-colnames(data3) <- data3[1, ] %>% as.character
-data3 <- data3 %>% slice(-1)
+data <- as.data.frame(t(data), stringsAsFactors = FALSE)
+
+# using the first row as column names
+colnames(data) <- data[1, ] %>% as.character
+data <- data %>% slice(-1)
 
 # removing old day and month
-data3[, 2:3] <- NULL
+data[, 2:3] <- NULL
 
 # transforming to date format
-data3[, 1] <- as.Date(data3[, 1], format = "%Y, %B, %d") 
+data[, 1] <- as.Date(data[, 1], format = "%Y, %B, %d") 
 
 # joining date and time
-data3$hour <- ifelse(data3$hour == "9:00 ", "09:00", data3$hour)
-data3$hour <- paste0(data3$hour, ":00")
-data3$time <- paste(data3[, 1], data3[, 2])
-data3$time <- as.POSIXct(data3$time)
+data$hour <- ifelse(data$hour == "9:00 ", "09:00", data$hour)
+data$hour <- paste0(data$hour, ":00")
+data$time <- paste(data[, 1], data[, 2])
+data$time <- as.POSIXct(data$time)
 
 # removing old day and time columns
-data3[, 1:2] <- NULL
-data3 <- data3[, c(ncol(data3), 1:(ncol(data3)-1))]
+data[, 1:2] <- NULL
+data <- data[, c(ncol(data), 1:(ncol(data) - 1) )]
 
 # matching contact with hours
-for (i in 2:ncol(data3) ) {
+for (i in 2:ncol(data) ) {
     
-    data3[, i] <- ifelse(data3[, i] == "OK", colnames(data3)[i], NA)
+    data[, i] <- ifelse(data[, i] == "OK", colnames(data)[i], NA)
 
 }
 
 # removing NAs
+ind <- apply(data[, 2:ncol(data)], 1, function(x) all(is.na(x) ) )
+data <- data[!ind, ]
 
-ind <- apply(data3[, 2:ncol(data3)], 1, function(x) all(is.na(x) ) )
-data4 <- data3[!ind, ]
-
-for (i in 1:nrow(data4) ) {
+for (i in 1:nrow(data) ) {
     
-    data4[i, ] <- data4[i, !is.na(data4[i, ])]
+    data[i, ] <- data[i, !is.na(data[i, ])]
     
 }
 
-data4 <- data4[, 1:2] %>% magrittr::set_colnames(c("time", "participant") )
+# keeping only the first two columns and renaming them
+data <- data[, 1:2] %>% magrittr::set_colnames(c("time", "participant") )
 
 # find participants with no choice
-
-ind <- apply(data[, 2:151], 1, function(x) all(is.na(x) ) )
-no_choices <- which(ind == TRUE)
+# ind <- apply(data[, 2:151], 1, function(x) all(is.na(x) ) )
+# no_choices <- which(ind == TRUE)
 
 # find participants with more than one choice
+# multiple_choices <- data[which(duplicated(data[, 1]) ), 1] %>% as.character
 
-multiple_choices <- data[which(duplicated(data[, 1]) ), 1] %>% as.character
-
-################################################################
+################################################
 # sending emails from R
-# https://github.com/jennybc/send-email-with-r
-#####################################################
+################################
 
-# if there is no multiple nor zero choice
+# title of the mail
+this_hw <- "Experience 'Validation d'un nouveau test de QI'"
 
-if (mean(ind) == 0 & is.na(multiple_choices) ) {
+# sender (you)
+email_sender <- "Ladislas Nalborczyk <ladislas.nalborczyk@gmail.com>"
+
+# optional bcc
+optional_bcc <- "Ladislas Nalborczyk <ladislas.nalborczyk@gmail.com>"
+
+# body of the mail
+body <- "Bonjour.
+
+Je vous confirme et rappelle votre inscription à l'expérience ..., le %s.
+
+L'expérience aura lieu ...
+
+Cordialement,
+
+l'équipe de recherche"
+
+# putting all the info in a single dataframe
+edat <-
+    data %>%
+    mutate(
+        To = sprintf("<%s>", participant),
+        Bcc = optional_bcc,
+        From = email_sender,
+        Subject = sprintf(this_hw),
+        body = sprintf(body, time)
+    )
+
+# identify participants registered for tomorrow
+today <- Sys.time() %>% as.Date
+tomorrow <- today + 1
+ppts <- which(as.Date(edat$time) == tomorrow+2) %>% as.numeric
+
+# if some participants are planned for tomorrow, send emails
+if (!is_empty(ppts) ) {
     
-    this_hw <- "Experience 'Validation d'un nouveau test de QI'"
-    email_sender <- "Ladislas Nalborczyk <ladislas.nalborczyk@gmail.com>"
-    optional_bcc <- "Ladislas Nalborczyk <ladislas.nalborczyk@gmail.com>"
-    body <- "Bonjour.
+    edat2 <- edat[ppts, ]
     
-    Je vous confirme et rappelle votre inscription à l'expérience 'Validation d'un nouveau test de QI', le %s.
+    emails <- edat2 %>% pmap(mime)
     
-    L'expérience aura lieu dans le box C du LPNC, au 1er étage du BSHM (Aile D).
-    
-    Cordialement,
-    
-    l'équipe de recherche"
-    
-    edat <-
-        data4 %>%
-        mutate(
-            To = sprintf("<%s>", participant),
-            Bcc = optional_bcc,
-            From = email_sender,
-            Subject = sprintf(this_hw),
-            body = sprintf(body, time)
-        )
-    
-    # saving emails
-    write.csv(edat, "composed-emails.csv")
-    
-    # identify participants registered for tomorrow
-    today <- Sys.time() %>% as.Date
-    tomorrow <- today + 1
-    ppts <- which(as.Date(edat$time) == tomorrow) %>% as.numeric
-    
-    # if participants planned for tomorrow, send emails
-    
-    if (!is_empty(ppts) ) {
-        
-        edat2 <- edat[ppts, ]
-        
-        emails <- edat2 %>% pmap(mime)
-        
-        safe_send_message <- safely(send_message)
-        sent_mail <- emails %>% map(safe_send_message)
-        
-    }
+    safe_send_message <- safely(send_message)
+    sent_mail <- emails %>% map(safe_send_message)
     
 }
 
+# quitting the sourced script
 quit(save = "no")
